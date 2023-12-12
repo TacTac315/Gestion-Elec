3f20301abaae18d3e7cf4eb084876678f609e01f
Vincent Eychenne <veychenn@etud.insa-toulouse.fr>
2023-12-12 14:35:22 +0100

    ajout code base

---
+++ PMSM_TGN002_cde_Uf_etudiant/Debug/src/Vector.d
@@ -0,0 +1,13 @@
+src/Vector.o: ../src/Vector.c \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/lib_c99.prefix \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/common.prefix \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_c_version.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf/include/compiler_api.h
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/lib_c99.prefix:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/common.prefix:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_c_version.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf/include/compiler_api.h:
---
+++ PMSM_TGN002_cde_Uf_etudiant/Debug/src/main.d
@@ -0,0 +1,115 @@
+src/main.o: ../src/main.c \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/lib_c99.prefix \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/common.prefix \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_c_version.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/MPC5744P.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/typedefs.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/stdint.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ansi_parms.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewlGlobals.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/pa/ansi_prefix.PA_EABI.bare.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/os_enum.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/cstdint \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/limits_api.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_lib_ext1.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/wchar_t.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/derivative.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/typedefs.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ClockConfig.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ModeEntryAndConfig.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ConfigGPIO.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ConfigPIT.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/Cmd_moteur.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/SWLIBS_Typedefs.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/SWLIBS_Config.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/SWLIBS_MacroDisp.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/SWLIBS_Typedefs.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/gd3000.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/pga411.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ConfigADC.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ConfigCTU.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/Current_Sensor.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/UARTConfig.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/FreeMASTER_Serial_Communication_Driver_V2_0/src_platforms/MPC57xx/freemaster.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/FreeMASTER_Serial_Communication_Driver_V2_0/src_common/freemaster_defcfg.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/freemaster_cfg.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/FreeMASTER_Serial_Communication_Driver_V2_0/src_common/freemaster_tsa.h \
+ C:/NXP/S32DS_Power_v2.1/S32DS/software/FreeMASTER_Serial_Communication_Driver_V2_0/src_common/freemaster_defcfg.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/User_Interface.h \
+ C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ConfigPWM.h
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/lib_c99.prefix:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/common.prefix:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_c_version.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/MPC5744P.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/typedefs.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/stdint.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ansi_parms.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewlGlobals.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/pa/ansi_prefix.PA_EABI.bare.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/os_enum.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/cstdint:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/limits_api.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/ewl_lib_ext1.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/build_tools/e200_ewl2/EWL_C/include/wchar_t.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/derivative.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/typedefs.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ClockConfig.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ModeEntryAndConfig.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ConfigGPIO.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ConfigPIT.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/Cmd_moteur.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/SWLIBS_Typedefs.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/SWLIBS_Config.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/SWLIBS_MacroDisp.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/SWLIBS_Typedefs.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/gd3000.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/pga411.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ConfigADC.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ConfigCTU.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/Current_Sensor.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/UARTConfig.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/FreeMASTER_Serial_Communication_Driver_V2_0/src_platforms/MPC57xx/freemaster.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/FreeMASTER_Serial_Communication_Driver_V2_0/src_common/freemaster_defcfg.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/freemaster_cfg.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/FreeMASTER_Serial_Communication_Driver_V2_0/src_common/freemaster_tsa.h:
+
+C:/NXP/S32DS_Power_v2.1/S32DS/software/FreeMASTER_Serial_Communication_Driver_V2_0/src_common/freemaster_defcfg.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/User_Interface.h:
+
+C:/Users/adminaboyer/alex/2023-24/BE_Elec_auto/Workspace/PMSM_TGN002_cde_Uf_etudiant/include/ConfigPWM.h:
